997cdfe400c3cd701025f4f94416c61edab8386b
Nick Schuch <nick@myschuch.com>
2024-07-06 18:10:50 +1000

    New collector binary: compass-find-lib

---
+++ collector/cmd/compass-find-lib/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/shirou/gopsutil/process"
+	"github.com/spf13/cobra"
+
+	"github.com/skpr/compass/collector/internal/envget"
+)
+
+var (
+	cmdLong = `
+		Helper script to discovery the parent pid ID by process name. Used by the collector.`
+
+	cmdExample = `
+		# Run the helper script and look for PHP FPM's parent process ID.
+		compass-find-lib --process-name=php-fpm
+
+		# Run the script with a custom lib path.
+		compass-find-lib --process-name=php-fpm --lib-path=/usr/lib/php/modules/something-else.so`
+)
+
+func main() {
+	var (
+		flagProcessName string
+		flagLibPath     string
+	)
+
+	cmd := &cobra.Command{
+		Use:                   "compass-find-lib",
+		DisableFlagsInUseLine: true,
+		Short:                 "Run the helper script",
+		Long:                  cmdLong,
+		Example:               cmdExample,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			pid, err := waitForParentProcess(flagProcessName, time.Second)
+			if err != nil {
+				return err
+			}
+
+			path := fmt.Sprintf("/proc/%d/root%s", pid, flagLibPath)
+
+			_, err = os.Stat(path)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(path)
+
+			return nil
+		},
+	}
+
+	cmd.PersistentFlags().StringVar(&flagProcessName, "process-name", envget.GetString("COMPASS_PROCESS_NAME", ""), "Name of the process which will be used for discovery")
+	cmd.PersistentFlags().StringVar(&flagLibPath, "lib-path", envget.GetString("COMPASS_LIB_PATH", "/usr/lib/php/modules/compass.so"), "Path to the Compass extension")
+
+	cmd.Execute()
+}
+
+// Helper function to wait for parent process and return the pid.
+func waitForParentProcess(name string, duration time.Duration) (int32, error) {
+	for {
+		time.Sleep(duration)
+
+		processes, err := process.Processes()
+		if err != nil {
+			return 0, fmt.Errorf("failed to get process list: %w", err)
+		}
+
+		pid, ok, err := findParentProcess(processes, name)
+		if err != nil {
+			return 0, fmt.Errorf("failed to find parent process from list: %w", err)
+		}
+
+		if !ok {
+			continue
+		}
+
+		return pid, nil
+	}
+}
+
+// Helper function to find the parent process
+func findParentProcess(list []*process.Process, name string) (int32, bool, error) {
+	for _, p := range list {
+		n, err := p.Name()
+		if err != nil {
+			return 0, false, fmt.Errorf("error getting process name: %w", err)
+		}
+
+		if n != name {
+			continue
+		}
+
+		children, err := p.Children()
+		if err != nil {
+			continue
+		}
+
+		if len(children) > 0 {
+			return p.Pid, true, nil
+		}
+	}
+
+	return 0, false, nil
+}
---
+++ collector/docker/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+PROCESS_NAME=$1
+
+compass-collector --lib-path=$(compass-find-lib)
