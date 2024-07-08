eeb60b46e3031a4aa8f070078d86379845ba11e3
Nick Schuch <nick@myschuch.com>
2024-07-08 22:12:06 +1000

    New plugin: noop

---
+++ collector/plugin/noop/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/skpr/compass/collector/internal/event/types"
+)
+
+// Plugin that is exported for use by the collector.
+var Plugin plugin
+
+type plugin struct{}
+
+func (s *plugin) Initialize() error {
+	return nil
+}
+
+// TraceEnd event from the collector.
+func (s *plugin) TraceEnd(trace types.Trace) error {
+	return nil
+}
