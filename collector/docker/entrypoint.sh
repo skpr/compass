3b239eaebf240fdff57d37c256c9d2f286a31d80
Nick Schuch <nick@previousnext.com.au>
2024-07-13 13:20:18 +1000

    Update entrypoint.sh

--- collector/docker/entrypoint.sh
+++ collector/docker/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sed "s~FILE~$(compass-find-lib --process-name=php-fpm)~g" php_functions.bt | bpftrace -
+sed "s~FILE~$(compass-find-lib --process-name=php-fpm)~g" /compass.bt | bpftrace -
