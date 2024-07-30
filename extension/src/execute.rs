e58f31403121d4943e069588977654b7ad31cc2b
Nick Schuch <nick@myschuch.com>
2024-07-30 10:55:11 +1000

    Fix

--- extension/src/execute.rs
+++ extension/src/execute.rs
@@ -60,7 +60,7 @@ unsafe extern "C" fn execute_ex(execute_data: *mut sys::zend_execute_data) {
         }
     };
 
-    if elapsed.as_nanos() < 100000 {
+    if elapsed.as_nanos() < 10000 {
         return;
     }
 
