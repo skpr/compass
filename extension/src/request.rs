6930f0aeb985e20fa218163748ea247a646a28d1
Nick Schuch <nick@myschuch.com>
2024-09-07 21:05:52 +1000

    Fix jit

--- extension/src/request.rs
+++ extension/src/request.rs
@@ -2,26 +2,12 @@ use crate::util::{
     get_header_key, get_request_id, get_request_server, get_sapi_module_name, jit_initialization,
 };
 
-use crate::{enabled, header};
-
-use crate::util;
+use crate::{header};
 
 use probe::probe;
 
 pub fn init() {
     if get_sapi_module_name().to_bytes() != b"fpm-fcgi" {
-        return;
-    }
-
-    let server_result = get_request_server();
-
-    let server = match server_result {
-        Ok(carrier) => carrier,
-        // @todo, This should not panic.
-        Err(error) => panic!("Problem getting the server: {:?}", error),
-    };
-
-    if header::block_execution(get_header_key(server)) {
         return;
     }
 
