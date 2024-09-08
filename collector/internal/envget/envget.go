c118f69480ecd95a9d57501a71621099e92d39d7
Nick Schuch <nick@myschuch.com>
2024-09-08 19:22:16 +1000

    Envs for threshold

--- collector/internal/envget/envget.go
+++ collector/internal/envget/envget.go
@@ -1,7 +1,10 @@
 // Package envget is used to get environment variables with fallbacks.
 package envget
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 // String from environment variable with fallback.
 func String(key, fallback string) string {
@@ -9,6 +12,20 @@ func String(key, fallback string) string {
 
 	if val != "" {
 		return val
+	}
+
+	return fallback
+}
+
+// Float64 returns an float64 env var.
+func Float64(key string, fallback float64) float64 {
+	if value, ok := os.LookupEnv(key); ok {
+		v, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			panic(err)
+		}
+
+		return v
 	}
 
 	return fallback
