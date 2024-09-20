8e4d3a908f2aebe6a8ba62d578eb6cd598856c1a
Nick Schuch <nick@myschuch.com>
2024-09-20 23:14:45 +1000

    tests

--- collector/cmd/compass/watch/app/breakdown/execution.go
+++ collector/cmd/compass/watch/app/breakdown/execution.go
@@ -7,5 +7,5 @@ import (
 
 // A graphical representation of the execution time.
 func getExecutionGraph(totalExecutionTime, executionTime float64) string {
-	return fmt.Sprintf("%s (%vms)", strings.Repeat("█", int(totalExecutionTime/executionTime*20)), totalExecutionTime)
+	return fmt.Sprintf("%s (%vms)", strings.Repeat("█", int(totalExecutionTime/executionTime*10)), totalExecutionTime)
 }
---
+++ collector/cmd/compass/watch/app/breakdown/position_test.go
@@ -0,0 +1,21 @@
+package breakdown
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetPositionStartAndEnd(t *testing.T) {
+	begin, end := getPositionStartAndEnd(0, 5, 10)
+	assert.Equal(t, 0, begin)
+	assert.Equal(t, 5, end)
+
+	begin, end = getPositionStartAndEnd(1, 5, 10)
+	assert.Equal(t, 1, begin)
+	assert.Equal(t, 6, end)
+
+	begin, end = getPositionStartAndEnd(7, 5, 10)
+	assert.Equal(t, 5, begin)
+	assert.Equal(t, 10, end)
+}
