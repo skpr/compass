1d7178ba0cb1c3a9fd74577efe146a1a2462b2d3
Nick Schuch <nick@myschuch.com>
2024-09-21 12:46:01 +1000

    Fix CLI execution

--- extension/src/execute.rs
+++ extension/src/execute.rs
@@ -24,12 +24,6 @@ pub fn register_exec_functions() {
 }
 
 unsafe extern "C" fn execute_ex(execute_data: *mut sys::zend_execute_data) {
-    // @todo, Consider making this work for other situations eg. CLI.
-    if get_sapi_module_name().to_bytes() != b"fpm-fcgi" {
-        upstream_execute_ex(None);
-        return;
-    }
-
     let execute_data = match ExecuteData::try_from_mut_ptr(execute_data) {
         Some(execute_data) => execute_data,
         None => {
@@ -37,6 +31,12 @@ unsafe extern "C" fn execute_ex(execute_data: *mut sys::zend_execute_data) {
             return;
         }
     };
+
+    // @todo, Consider making this work for other situations eg. CLI.
+    if get_sapi_module_name().to_bytes() != b"fpm-fcgi" {
+        upstream_execute_ex(Some(execute_data));
+        return;
+    }
 
     let server_result = get_request_server();
 
