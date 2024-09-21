31d299b5d1283798f7f791dff462e6803a744c5d
Nick Schuch <nick@previousnext.com.au>
2024-09-21 23:42:24 +1000

    Conslidate parallel tasks

--- collector/pkg/tracing/aggregated/complete.go
+++ collector/pkg/tracing/aggregated/complete.go
@@ -12,7 +12,34 @@ func FromCompleteProfile(upstream complete.Profile) Profile {
 		Functions:     make(map[string]Function),
 	}
 
+	// Consolidate function call that might have occurred int parallel.
+	var consolidated []complete.FunctionCall
+
 	for _, call := range upstream.FunctionCalls {
+		if !listContains(consolidated, call.Name) {
+			consolidated = append(consolidated, call)
+			continue
+		}
+
+		for i, c := range consolidated {
+			if c.Name != call.Name {
+				continue
+			}
+
+			if call.StartTime > c.StartTime && call.StartTime < c.EndTime {
+				consolidated[i].EndTime = c.EndTime
+				continue
+			}
+
+			if call.EndTime > c.StartTime && call.EndTime < c.EndTime {
+				consolidated[i].StartTime = c.StartTime
+				continue
+			}
+		}
+	}
+
+	// Use the consolidated function calls to generated our new aggregated list.
+	for _, call := range consolidated {
 		if _, ok := profile.Functions[call.Name]; !ok {
 			profile.Functions[call.Name] = Function{
 				Name:          call.Name,
@@ -30,4 +57,14 @@ func FromCompleteProfile(upstream complete.Profile) Profile {
 	}
 
 	return profile
+}
+
+func listContains(list []complete.FunctionCall, name string) bool {
+	for _, item := range list {
+		if item.Name == name {
+			return true
+		}
+	}
+
+	return false
 }
