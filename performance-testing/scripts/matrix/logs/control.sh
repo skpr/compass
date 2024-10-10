e7a32c46584bbb74ae1e3bdbba8ee575a4660e60
Nick Schuch <nick@previousnext.com.au>
2024-10-10 12:58:25 +1000

    Fixing logs

---
+++ performance-testing/scripts/matrix/logs/control.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+OUTPUT_DIR=$1
+
+docker compose logs nginx > ${OUTPUT_DIR}/nginx.log
+docker compose logs php-fpm > ${OUTPUT_DIR}/php-fpm.log
+docker compose logs mysql-default > ${OUTPUT_DIR}/mysql-default.log
