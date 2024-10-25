33f41af3f0f31fba8c844d11b01e3cb89342b71c
Nick <nick@myschuch.com>
2024-10-25 21:10:07 +1000

    Fix validation

--- collector/internal/collector/program.bpf.c
+++ collector/internal/collector/program.bpf.c
@@ -3,23 +3,22 @@
 // # $ readelf -n /usr/lib/php/modules/compass.so
 //
 // Displaying notes found in: .note.gnu.build-id
-//   Owner                Data size 	Description
-//   GNU                  0x00000014	NT_GNU_BUILD_ID (unique build ID bitstring)
-//     Build ID: cb4d92781fc5c3a8b8db668910840b04762e4104
-// 
+// Owner                Data size 	Description
+// GNU                  0x00000014	NT_GNU_BUILD_ID (unique build ID bitstring)
+//   Build ID: f3292e5e81429fcc9d40f29eaaff2c4789aae17c
+//
 // Displaying notes found in: .note.stapsdt
 //   Owner                Data size 	Description
-//   stapsdt              0x00000045	NT_STAPSDT (SystemTap probe descriptors)
-//     Provider: compass
-//     Name: php_function
-//     Location: 0x000000000000fa11, Base: 0x000000000005e9eb, Semaphore: 0x0000000000000000
-//     Arguments: -8@%rdi -8@%rbx -8@%r14 -8@%r15
-                  -8@%rdi -8@%r14 -8@%r15 -8@%rbx
-//   stapsdt              0x00000039	NT_STAPSDT (SystemTap probe descriptors)
-//     Provider: compass
-//     Name: request_shutdown
-//     Location: 0x000000000000fe15, Base: 0x000000000005e9eb, Semaphore: 0x0000000000000000
-//     Arguments: -8@%rdi
+// stapsdt              0x00000039	NT_STAPSDT (SystemTap probe descriptors)
+//   Provider: compass
+//   Name: request_shutdown
+//   Location: 0x000000000000cd48, Base: 0x0000000000064517, Semaphore: 0x0000000000000000
+//   Arguments: -8@%rdi
+// stapsdt              0x0000004d	NT_STAPSDT (SystemTap probe descriptors)
+//   Provider: compass
+//   Name: php_function
+//   Location: 0x000000000000e62a, Base: 0x0000000000064517, Semaphore: 0x0000000000000000
+//   Arguments: -8@%rdi -8@%r14 -8@%r15 -8@%rbx
 
 #define STRSZ 100 + 1
 
