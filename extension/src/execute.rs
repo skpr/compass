8e8c8e058b14954a62ce9d6c2d2b45ff6cca8d8b
Nick <nick@myschuch.com>
2024-11-02 14:02:27 +1000

    Remove header check

--- extension/src/execute.rs
+++ extension/src/execute.rs
@@ -50,13 +50,43 @@ unsafe extern "C" fn execute_ex(execute_data: *mut sys::zend_execute_data) {
         }
     };
 
-    if header::block_execution(get_header_key(server)) {
-        upstream_execute_ex(Some(execute_data));
+    let (function_name, class_name) = match get_function_and_class_name(execute_data) {
+        Ok(x) => x,
+        Err(_err) => {
+            error!("failed to get class and function name: {}", _err);
+            upstream_execute_ex(Some(execute_data));
+            return;
+        }
+    };
+
+    let function_name: String = function_name.map(|f| f.to_string()).unwrap_or_default();
+    let class_name: String = class_name.map(|c| c.to_string()).unwrap_or_default();
+    let combined_name = get_combined_name(class_name, function_name);
+
+    let start = get_unix_timestamp_micros();
+
+    // Run the upstream function.
+    upstream_execute_ex(Some(execute_data));
+
+    let end = get_unix_timestamp_micros();
+
+    if block_probe_event(
+        mode::header_enabled(),
+        threshold::is_under_function_threshold(end - start),
+    ) {
         return;
     }
 
-    upstream_execute_ex(Some(execute_data));
-    return;
+    let request_id = get_request_id(server);
+
+    probe!(
+        compass,
+        php_function,
+        request_id.as_ptr(),
+        combined_name.as_ptr(),
+        start,
+        end,
+    );
 }
 
 // Helper function to allow all probes if the header mode is enabled.
