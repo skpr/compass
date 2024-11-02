a95ccfd85ddf3a84e309bd194f9e80da44aa55b6
Nick <nick@myschuch.com>
2024-11-02 15:23:24 +1000

    Tings

--- extension/src/util.rs
+++ extension/src/util.rs
@@ -22,9 +22,9 @@ pub fn get_function_and_class_name(
 
     let class_name = function
         .get_class()
-        .map(|cls| cls.get_name().to_str().map(ToOwned::to_owned))
-        .transpose()
-        .expect("class name not found");
+        .map(ZStr::to_str)
+        .transpose()?
+        .map(ToOwned::to_owned);
 
     let function_name = function
         .get_function_name()
