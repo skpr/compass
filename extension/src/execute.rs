cc462c6947908bb8e896452e2a8c4f71fd665d2b
Nick <nick@myschuch.com>
2024-11-02 15:02:13 +1000

    class name check

--- extension/src/execute.rs
+++ extension/src/execute.rs
@@ -53,7 +53,6 @@ unsafe extern "C" fn execute_ex(execute_data: *mut sys::zend_execute_data) {
     let (function_name, class_name) = match get_function_and_class_name(execute_data) {
         Ok(x) => x,
         Err(_err) => {
-            error!("failed to get class and function name: {}", _err);
             upstream_execute_ex(Some(execute_data));
             return;
         }
