bdc116227be90841284c03f6a09165c0cb99a83f
Nick <nick@myschuch.com>
2024-11-02 13:20:12 +1000

    Remove collector

--- .github/workflows/performance/matrix/collector/logs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-OUTPUT_DIR=$1
-
-docker compose logs nginx > ${OUTPUT_DIR}/nginx.log
-docker compose logs php-fpm > ${OUTPUT_DIR}/php-fpm.log
-docker compose logs mysql-default > ${OUTPUT_DIR}/mysql-default.log
-docker compose logs collector > ${OUTPUT_DIR}/collector.log
