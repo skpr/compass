641dafc9e79124a2be20c7f75143d57c1fc3b4c5
Nick <nick@myschuch.com>
2025-05-14 22:17:26 +1000

    eBPF perform updates

--- collector/template.bpf.c
+++ collector/template.bpf.c
@@ -10,23 +10,22 @@
 
 char __license[] SEC("license") = "Dual MIT/GPL";
 
-const char event_type_function[] = "function";
-
-const char event_type_request_init[] = "request_init";
-
-const char event_type_request_shutdown[] = "request_shutdown";
-
-struct event {
-  u8 type[STRSZ];
-  u8 request_id[STRSZ];
-  u8 uri[URI_MAX_LEN];
-  u8 method[STRSZ];
-  u8 function_name[STRSZ];
-  u64 timestamp;
-  u64 elapsed;
+enum event_type : __u8 {
+    EVENT_TYPE_FUNCTION = 0,
+    EVENT_TYPE_REQUEST_INIT = 1,
+    EVENT_TYPE_REQUEST_SHUTDOWN = 2,
 };
 
-// Force emitting structs into the ELF.
+struct event {
+    __u8 type;
+    __u8 request_id[STRSZ];
+    __u8 method[STRSZ];
+    __u8 function_name[STRSZ];
+    __u8 uri[URI_MAX_LEN];
+    __u64 timestamp;
+    __u64 elapsed;
+};
+
 const struct event *unused_event __attribute__((unused));
 
 struct {
@@ -34,73 +33,55 @@ struct {
     __uint(max_entries, 256 * 4096);
 } events SEC(".maps");
 
-// A canary function for enabling probes in the extension.
-// This is now we achieve a zero to no performance impact when probes are not enabled.
 SEC("uprobe/compass_canary")
 int uprobe_compass_canary(struct pt_regs *ctx) {
-  return 0;
+    return 0;
 }
 
-// Used to inform the user space application that a request initialised.
 SEC("uprobe/compass_request_init")
 int uprobe_compass_request_init(struct pt_regs *ctx) {
-  struct event *event;
+    struct event *event = bpf_ringbuf_reserve(&events, sizeof(*event), 0);
+    if (!event)
+        return 0;
 
-  event = bpf_ringbuf_reserve(&events, sizeof(struct event), 0);
-  if (!event)
+    event->type = EVENT_TYPE_REQUEST_INIT;
+    bpf_probe_read_user(&event->request_id, STRSZ, (void *)ctx->REQUEST_INIT_ARG_REQUEST_ID);
+    bpf_probe_read_user(&event->method, STRSZ, (void *)ctx->REQUEST_INIT_ARG_METHOD);
+    bpf_probe_read_user(&event->uri, URI_MAX_LEN, (void *)ctx->REQUEST_INIT_ARG_URI);
+    event->timestamp = bpf_ktime_get_ns();
+    event->elapsed = 0;
+
+    bpf_ringbuf_submit(event, 0);
     return 0;
-
-  // Add in the extra call information.
-  bpf_core_read(&event->type, STRSZ, &event_type_request_init);
-  bpf_probe_read_user_str(&event->request_id, STRSZ, (void *)ctx->REQUEST_INIT_ARG_REQUEST_ID);
-  bpf_probe_read_user_str(&event->uri, URI_MAX_LEN, (void *)ctx->REQUEST_INIT_ARG_URI);
-  bpf_probe_read_user_str(&event->method, STRSZ, (void *)ctx->REQUEST_INIT_ARG_METHOD);
-  event->timestamp = bpf_ktime_get_ns();
-
-  // Send it up to user space.
-  bpf_ringbuf_submit(event, 0);
-
-  return 0;
 }
 
-// Used to wrap up and send function execution data.
 SEC("uprobe/compass_php_function")
 int uprobe_compass_php_function(struct pt_regs *ctx) {
-  struct event *event;
+    struct event *event = bpf_ringbuf_reserve(&events, sizeof(*event), 0);
+    if (!event)
+        return 0;
 
-  event = bpf_ringbuf_reserve(&events, sizeof(struct event), 0);
-  if (!event)
+    event->type = EVENT_TYPE_FUNCTION;
+    bpf_probe_read_user(&event->request_id, STRSZ, (void *)ctx->PHP_FUNCTION_ARG_REQUEST_ID);
+    bpf_probe_read_user(&event->function_name, STRSZ, (void *)ctx->PHP_FUNCTION_ARG_FUNCTION_NAME);
+    event->timestamp = bpf_ktime_get_ns();
+    event->elapsed = ctx->PHP_FUNCTION_ARG_ELAPSED;
+
+    bpf_ringbuf_submit(event, 0);
     return 0;
-
-  // Add in the extra call information.
-  bpf_core_read(&event->type, STRSZ, &event_type_function);
-  bpf_probe_read_user_str(&event->request_id, STRSZ, (void *)ctx->PHP_FUNCTION_ARG_REQUEST_ID);
-  bpf_probe_read_user_str(&event->function_name, STRSZ, (void *)ctx->PHP_FUNCTION_ARG_FUNCTION_NAME);
-  event->timestamp = bpf_ktime_get_ns();
-  event->elapsed = ctx->PHP_FUNCTION_ARG_ELAPSED;
-
-  // Send it up to user space.
-  bpf_ringbuf_submit(event, 0);
-
-  return 0;
 }
 
-// Used to inform the user space application that a request has shutdown.
 SEC("uprobe/compass_request_shutdown")
 int uprobe_compass_request_shutdown(struct pt_regs *ctx) {
-  struct event *event;
+    struct event *event = bpf_ringbuf_reserve(&events, sizeof(*event), 0);
+    if (!event)
+        return 0;
 
-  event = bpf_ringbuf_reserve(&events, sizeof(struct event), 0);
-  if (!event)
+    event->type = EVENT_TYPE_REQUEST_SHUTDOWN;
+    bpf_probe_read_user(&event->request_id, STRSZ, (void *)ctx->REQUEST_SHUTDOWN_ARG_REQUEST_ID);
+    event->timestamp = bpf_ktime_get_ns();
+    event->elapsed = 0;
+
+    bpf_ringbuf_submit(event, 0);
     return 0;
-
-  // Add in the extra call information.
-  bpf_core_read(&event->type, STRSZ, &event_type_request_shutdown);
-  bpf_probe_read_user_str(&event->request_id, STRSZ, (void *)ctx->REQUEST_SHUTDOWN_ARG_REQUEST_ID);
-  event->timestamp = bpf_ktime_get_ns();
-
-  // Send it up to user space.
-  bpf_ringbuf_submit(event, 0);
-
-  return 0;
 }
