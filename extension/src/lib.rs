f2901d0a6ac760be889975dda4df62bc94da9751
Nick Schuch <nick@previousnext.com.au>
2025-05-15 14:17:38 +1000

    Bump function threshold to 5000000

--- extension/src/lib.rs
+++ extension/src/lib.rs
@@ -16,7 +16,7 @@ pub fn get_module() -> Module {
     );
 
     module.add_ini(enabled::INI_CONFIG, false, Policy::All);
-    module.add_ini(threshold::INI_CONFIG, 1000000, Policy::All);
+    module.add_ini(threshold::INI_CONFIG, 5000000, Policy::All);
 
     module.on_module_init(on_module_init);
 
