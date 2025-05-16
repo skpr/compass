331b21a793628f20ac52d420e1a7d3a54b6a497d
Nick Schuch <nick@previousnext.com.au>
2025-05-16 13:17:57 +1000

    Disable cache for more accurate numbers

--- .github/workflows/performance/scripts/k6_run.sh
+++ .github/workflows/performance/scripts/k6_run.sh
@@ -4,4 +4,4 @@ SCENARIO=$1
 SUMMARY_EXPORT_DIRECTORY=$2
 SUMMARY_EXPORT_FILE_NAME=$3
 
-docker run --rm --network=host -v ${SUMMARY_EXPORT_DIRECTORY}:/results -i docker.io/grafana/k6 run - --summary-export=/results/${SUMMARY_EXPORT_FILE_NAME} --vus 2 --duration 30s < ${SCENARIO}
+docker run --rm --network=host -v ${SUMMARY_EXPORT_DIRECTORY}:/results -i docker.io/grafana/k6 run - --summary-export=/results/${SUMMARY_EXPORT_FILE_NAME} --vus 1 --duration 30s < ${SCENARIO}
