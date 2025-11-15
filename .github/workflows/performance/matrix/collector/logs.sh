3c3885355710bdebf114f99b096f690176d1e59f
Nick <nick@myschuch.com>
2025-11-15 23:33:36 +1000

    Logs fix

--- .github/workflows/performance/matrix/collector/logs.sh
+++ .github/workflows/performance/matrix/collector/logs.sh
@@ -5,4 +5,5 @@ OUTPUT_DIR=$1
 docker compose logs nginx > ${OUTPUT_DIR}/nginx.log
 docker compose logs php-fpm > ${OUTPUT_DIR}/php-fpm.log
 docker compose logs mysql-default > ${OUTPUT_DIR}/mysql-default.log
-docker compose logs collector > ${OUTPUT_DIR}/collector.log
+docker compose logs compass > ${OUTPUT_DIR}/compass.log
+docker compose logs curl > ${OUTPUT_DIR}/curl.log
